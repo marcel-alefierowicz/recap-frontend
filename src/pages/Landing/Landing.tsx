cf662e90528b1be34b91e1eff3cc1cfdc4a5109e
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2024-06-02 16:06:19 +0200

    added a landing page on '/', modified css slighty (still full of bloat)

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,11 +1,34 @@
-import './landing.css';
+import "./landing.css";
+import { Link } from "react-router-dom";
+
+
 
 export const Landing = () => {
   return (
-    <div className='page'>
-        <div className='helo'>
-            helleo bro
+    <div className="page">
+      <div className="container">
+        <div className="boxtop">
+          <p>pick your poison</p>
         </div>
+        <div className="boxcenter">
+            <div className="linkcontainer">
+                <div className="link">
+                    <Link to={'/login'}>log into your account</Link>
+                </div>
+            </div>
+            <div className="spacer">
+                <p>or</p>
+            </div>
+            <div className="linkcontainer">
+            <div className="link">
+                    <Link to={'/register'}>create a new one</Link>
+                </div>
+            </div>
+        </div>
+        <div className="boxbottom">
+            <p>made by two of the best web developers on the PLANET please hire me</p>
+        </div>
+      </div>
     </div>
-    );
+  );
 };
