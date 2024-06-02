37d1ede088d16d29974a5251bfd87a309f491b33
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2024-06-02 17:09:09 +0200

    added react hook form created register page and route

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,8 +1,6 @@
 import "./landing.css";
 import { Link } from "react-router-dom";
 
-
-
 export const Landing = () => {
   return (
     <div className="page">
@@ -11,22 +9,24 @@ export const Landing = () => {
           <p>pick your poison</p>
         </div>
         <div className="boxcenter">
-            <div className="linkcontainer">
-                <div className="link">
-                    <Link to={'/login'}>log into your account</Link>
-                </div>
-            </div>
-            <div className="spacer">
-                <p>or</p>
-            </div>
-            <div className="linkcontainer">
+          <div className="linkcontainer">
             <div className="link">
-                    <Link to={'/register'}>create a new one</Link>
-                </div>
+              <Link to={"/login"}>log into your account</Link>
             </div>
+          </div>
+          <div className="spacer">
+            <p>or</p>
+          </div>
+          <div className="linkcontainer">
+            <div className="link">
+              <Link to={"/register"}>create a new one</Link>
+            </div>
+          </div>
         </div>
         <div className="boxbottom">
-            <p>made by two of the best web developers on the PLANET please hire me</p>
+          <p>
+            made by two of the best web developers on the PLANET please hire me
+          </p>
         </div>
       </div>
     </div>
